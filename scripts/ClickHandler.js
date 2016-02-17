9514147a1aed7230042586f5a69b9491dfd2abc7
Joe B <mothman147@gmail.com>
2016-02-16 22:49:06 -0500

    css &html "done" buttons implemented currently do nothing :(
    ask people how to get buttons to work

---
+++ scripts/ClickHandler.js
@@ -0,0 +1,15 @@
+/**
+ AUTHOR: Joe Barbercheck
+ VERSION: 1.0
+ CREATED: 2-12-16
+ ASSIGNMENT: Text Adventure
+ */
+"use strict";
+
+export default class {ClickHandler};
+
+
+    constructor(); {
+
+    }
+
